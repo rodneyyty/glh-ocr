8e887356b9fe3a687cba8884df13532d10e8c2a8
lentan <lentan1029@gmail.com>
2018-02-23 21:53:45 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import pytesseract
+
+from PIL import Image
+import pytesseract
+
+im = Image.open("./test.jpg")
+
+text = pytesseract.image_to_string(im, lang = 'eng')
+
+print(text)
