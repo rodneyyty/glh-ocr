1b5dc1b25c4af040336a14a585fd060fc4c86d64
chongcher <ccchia.2014@smu.edu.sg>
2018-02-25 02:36:48 +0800

    Fix document classification

--- classify_docs/classifier.py
+++ classify_docs/classifier.py
@@ -1,17 +1,24 @@
 import sys
-from utility import get_abs_path_from_args, get_tagged_docs
-from gensim.models.doc2vec import Doc2Vec
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import os, io
 
 
 def main(args):
-    [dir_arg] = get_abs_path_from_args(args, 1)
-    train_corpus = get_tagged_docs(dir_arg)
-    model = Doc2Vec(vector_size=300, min_count=2, epochs=10)
-    model.build_vocab(train_corpus)
-    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs, workers=24)
+    corpus_file = args[0]
+    if not os.path.isfile(corpus_file):
+        exit(1)
+    corpus = []
+    with io.open(corpus_file, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            label, words = line.split(' ', 1)
+            corpus.append(TaggedDocument(words.strip().split(), [label]))
+    model = Doc2Vec(vector_size=300, min_count=2, iter=3, workers=24)
+    model.build_vocab(corpus)
+    model.train(corpus, total_examples=model.corpus_count, epochs=10)
     model.save("lawinsider.model")
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-# nohup python -u classifier.py ../data/lawinsider/ &> classifier.out &
+# nohup python -u classifier.py /home/ccchia.2014/glh-ocr/data/lawinsider/ &> classifier.out &
+# python classifier.py C:\Users\chongcher\Documents\glh-ocr\data\lawinsider_2
