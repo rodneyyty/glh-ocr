de3feea1d0b57d677e4c5d6b6916a8fc4d1406e6
lentan <lentan1029@gmail.com>
2018-03-11 05:17:32 +0800

    Some more thingy

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-app.use(express.static('vis'))
+app.use('/data', express.static('data/IRAS'))
+app.get('/datas.json', (req, res) => {
+  res.sendFile(path.join(__dirname + '/vis/datas.json'))
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/vis/index.html'))
+});
 app.listen(3000)
