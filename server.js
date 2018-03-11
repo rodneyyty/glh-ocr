8cc68de3cf77e751e20ece4d03d4a62db7e43520
lentan <lentan1029@gmail.com>
2018-03-11 20:29:32 +0800

    Change location of datas.json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use('/data', express.static('data/IRAS'))
 app.get('/datas.json', (req, res) => {
-  res.sendFile(path.join(__dirname + '/vis/datas.json'))
+  res.sendFile(path.join(__dirname + '/data/datas.json'))
 })
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/vis/index.html'))
