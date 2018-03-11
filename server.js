cc3b2ce9dfc80b9b453b28c9cf65a47061049bd2
lentan <lentan1029@gmail.com>
2018-03-12 00:39:28 +0800

    Change path for server static files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-app.use('/data', express.static('data/IRAS'))
+app.use('/data', express.static('data/Demo Library'))
 app.get('/datas.json', (req, res) => {
   res.sendFile(path.join(__dirname + '/vis/datas.json'))
 })
